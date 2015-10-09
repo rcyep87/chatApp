9d68d12af319af83838a407c10f7134d23494dca
Ryan Yep <ryan.yep@gmail.com>
2015-10-09 11:58:22 -0500

    Migrated to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,18 @@
 
 ActiveRecord::Schema.define(version: 20151009161141) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "message"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+
   create_table "messages", force: :cascade do |t|
     t.string   "msg"
     t.integer  "user_id"
@@ -21,13 +33,28 @@ ActiveRecord::Schema.define(version: 20151009161141) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "messages", ["room_id"], name: "index_messages_on_room_id"
-  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
+  add_index "messages", ["room_id"], name: "index_messages_on_room_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.boolean  "published",  default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "taskmanagers", force: :cascade do |t|
+    t.string   "task",       limit: 300
+    t.boolean  "completed",              default: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +66,7 @@ ActiveRecord::Schema.define(version: 20151009161141) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
