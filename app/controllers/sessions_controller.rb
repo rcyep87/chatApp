94d838532ccec0fdd63cdced0fcf8ec094ec382a
Travis Montgomery <travisbm@gmail.com>
2015-10-09 13:06:40 -0500

    authentication working. login form up. sign up form up.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:username])
     if user.present? && user.authenticate(params[:password])
       set_user_session(user) #creates temporary cookie
-      redirect_to root_path, notice: "Successfully logged in!"
+      redirect_to rooms_path, notice: "Successfully logged in!"
     else
       flash[:alert] = "Username or Password did not match"
       render :new
