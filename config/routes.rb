8e6917f9a7c0a0d837b28b63606e9c5913f2f384
Ryan Yep <ryan.yep@gmail.com>
2015-10-09 11:48:28 -0500

    Remove password_digest from user.rb strong param

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :rooms
   resources :users
 
-  get  'login', to: 'sessions#new',    as: 'login'
-  post 'login', to: 'sessions#create', as: 'create_session'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get  'login'  , to: 'sessions#new'    , as: 'login'
+  post 'login'  , to: 'sessions#create' , as: 'create_session'
+  get  'logout' , to: 'sessions#destroy', as: 'logout'
+  
 end
